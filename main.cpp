673cb53084dee9a7f2131c48c9d515596c068441
salomonhippie <salomonhippie@gmail.com>
2010-08-05 07:44:08 +0000

    some main stuff + added Wii extension

--- main.cpp
+++ main.cpp
@@ -7,18 +7,28 @@
 // See the GNU General Public License for more details (see LICENSE). 
 //--------------------------------------------------------------------
 
-// OpenEngine stuff
+// OpenEngine Core stuff
 #include <Logging/Logger.h>
 #include <Logging/StreamLogger.h>
 #include <Core/Engine.h>
 
+// Extensions
+#include <Display/WiiEnvironment.h>
+#include <Display/WiiCanvas.h>
+#include <Display/ViewingVolume.h>
+#include <Scene/SceneNode.h>
+#include <Renderers/WiiRenderer.h>
+#include <Renderers/WiiRenderingView.h>
+
 #include <stdio.h>
 #include <stdlib.h>
-// #include <cstdlib>
 
 using namespace OpenEngine::Logging;
 using namespace OpenEngine::Core;
 using namespace OpenEngine::Utils;
+using namespace OpenEngine::Display;
+using namespace OpenEngine::Renderers;
+using namespace OpenEngine::Scene;
 
 int main(int argc, char** argv) {
     // Setup logging facilities.
@@ -26,6 +36,32 @@ int main(int argc, char** argv) {
 
     // Print usage info.
     logger.info << "========= Running OpenEngine Nintendo Wii Demo Project =========" << logger.end;
+
+    IEngine* engine       = new Engine();
+    IEnvironment* env     = new WiiEnvironment();
+    IFrame& frame         = env->CreateFrame();
+    IRenderCanvas* canvas = new WiiCanvas();
+    IRenderer* renderer   = new WiiRenderer();
+    IRenderingView* rv    = new WiiRenderingView();
+    IViewingVolume* vv    = new ViewingVolume();
+    ISceneNode* scene     = new SceneNode();
+
+    canvas->SetScene(scene);
+    canvas->SetViewingVolume(vv);
+
+    // renderer->ProcessEvent().Attach(*rv);
+    //canvas->SetRenderer(renderer);
+    frame.SetCanvas(canvas);
+
+    // Bind to the engine for processing time
+    engine->InitializeEvent().Attach(*env);
+    engine->ProcessEvent().Attach(*env);
+    engine->DeinitializeEvent().Attach(*env);
+    
+    engine->Start();
+
+    delete engine;
+    delete env;
 
     // Return when the engine stops.
     return EXIT_SUCCESS;
